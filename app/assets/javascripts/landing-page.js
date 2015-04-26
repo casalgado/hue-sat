afbc5aebb9339a73e9e93d3482fe7953aad8fb4b
Carlos Salgado <casalgado86@yahoo.com>
2015-04-26 00:43:54 -0500

    landing page 2

--- app/assets/javascripts/landing-page.js
+++ app/assets/javascripts/landing-page.js
@@ -2,6 +2,8 @@
 var ready = function() {
 
 
+  $('#fade-08').hide()
+
     $('#fade-07').on('mousemove', function() {
         $('#fade-07').delay(1000).fadeOut(1500);
         $('#fade-06').delay(2000).fadeOut(1500);
@@ -9,6 +11,7 @@ var ready = function() {
         $('#fade-04').delay(4000).fadeOut(1500);
         $('#fade-03').delay(5000).fadeOut(1500);
         $('#fade-02').delay(6000).fadeOut(1500);
+        $('#fade-08').delay(7000).fadeIn(1000);
     });
 
 };
