0dc9c4d3546541b6c70171c6bd0f24c68b273021
Carlos Salgado <casalgado86@yahoo.com>
2015-04-27 10:08:06 -0500

    coming soon done

--- app/assets/javascripts/landing-page.js
+++ app/assets/javascripts/landing-page.js
@@ -4,15 +4,15 @@ var ready = function() {
 
   $('#fade-08').hide()
 
-    $('#fade-07').on('mousemove', function() {
+    
         $('#fade-07').delay(1000).fadeOut(1500);
         $('#fade-06').delay(2000).fadeOut(1500);
         $('#fade-05').delay(3000).fadeOut(1500);
         $('#fade-04').delay(4000).fadeOut(1500);
-        $('#fade-03').delay(5000).fadeOut(1500);
-        $('#fade-02').delay(6000).fadeOut(1500);
-        $('#fade-08').delay(7000).fadeIn(1000);
-    });
+        $('#fade-03').delay(4500).fadeOut(1500);
+        $('#fade-02').delay(5000).fadeOut(1500);
+        $('#fade-08').delay(6000).fadeIn(1000);
+
 
 };
 
