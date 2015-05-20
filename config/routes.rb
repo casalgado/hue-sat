82095f146aa4e25cec0c6a7163b607b18e9b69d3
casalgado <casalgado86@yahoo.com>
2015-05-20 11:03:34 -0500

    styled home button

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   
   
   devise_for :users
+  
   resources :pages do
   	get 'home', on: :collection
   	get 'portfolio', on: :collection
   	get 'blog', on: :collection
   	get 'about', on: :collection
-
-
   end
+
   root to: 'pages#show'
 
 end
